cfdcb91c9e4008f9fde2d3f987a6597639d97f09
Julio Cesar Santos de Vasconcelos <julioc43@gmail.com>
2022-02-06 11:33:11 -0300

    implementando API Vibration ao projeto

--- src/componets/Form/index.js
+++ src/componets/Form/index.js
@@ -1,62 +1,80 @@
-import React, {useState} from "react"
-import { View, Text, TextInput, TouchableOpacity } from "react-native"
+import React, { useState } from "react"
+import {
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
+    Vibration
+} from "react-native"
 import styles from "./style";
 import ResultImc from "./ResultIMC";
 
-export default function Form(){
+export default function Form() {
 
-const [height, setHeight]= useState(null)
-const [weight, setWeight]= useState(null)
-const [messageImc, setMessageImc]= useState("Preencha o peso e altura!");
-const [imc, setImc]= useState(null)
-const [textButton, setTextButton]= useState("Calcular")
+    const [height, setHeight] = useState(null)
+    const [weight, setWeight] = useState(null)
+    const [messageImc, setMessageImc] = useState("Preencha o peso e altura!");
+    const [imc, setImc] = useState(null)
+    const [textButton, setTextButton] = useState("Calcular")
+    const [errorMessage, setErrorMessage] = useState(null)
 
-function imcCalculator(){
-    return setImc((weight/(height*height)).toFixed(2))
-}
-
-function validationImc(){
-    if(weight != null && height != null){
-        imcCalculator()
-        setHeight(null)
-        setWeight(null)
-        setMessageImc("Seu IMC é igual:")
-        setTextButton("Calcular Novamente")
-        return
+    function imcCalculator() {
+        return setImc((weight / (height * height)).toFixed(2))
     }
-    setImc(null)
-    setTextButton("Calcular")
-    setMessageImc("Preencha o peso e altura!!!")
-}
 
-    return(
-       <View style={styles.formContext}>
-           <View style={styles.form}>
-               <Text style={styles.formLabel}>Altura</Text>
-               <TextInput
-               style={styles.input}
-               onChangeText={setHeight}
-               value={height}
-               placeholder="Ex. 1.75"
-               keyboardType="numeric"
-               />
+    function verificationImc() {
+        if (imc => null){
+            Vibration.vibrate();
+            setErrorMessage("Campo obrigatório*")
+        }
+    }
 
-               <Text style={styles.formLabel}>Peso</Text>
-               <TextInput
-               style={styles.input}
-               onChangeText={setWeight}
-               value={weight}
-               placeholder="Ex. 75.365"
-               keyboardType="numeric"
-               />
-               <TouchableOpacity style={styles.buttonCalculator}
-               onPress={() =>{
-                validationImc()
-               }}>
-               <Text style={styles.textButtonCalculator}>{textButton}</Text>
-               </TouchableOpacity>
-           </View>
-           <ResultImc messageResultImc={messageImc} resultImc={imc} />
-       </View>
+    function validationImc() {
+        if (weight != null && height != null) {
+            imcCalculator()
+            setHeight(null)
+            setWeight(null)
+            setMessageImc("Seu IMC é igual:")
+            setTextButton("Calcular Novamente")
+            setErrorMessage(null)
+            return
+        }
+        verificationImc()
+        setImc(null)
+        setTextButton("Calcular")
+        setMessageImc("Preencha o peso e altura!!!")
+    }
+
+    return (
+        <View style={styles.formContext}>
+            <View style={styles.form}>
+                <Text style={styles.formLabel}>Altura</Text>
+                <Text style={styles.errorMessage}>{errorMessage}</Text>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={setHeight}
+                    value={height}
+                    placeholder="Ex. 1.75"
+                    keyboardType="numeric"
+                />
+
+                <Text style={styles.formLabel}>Peso</Text>
+                <Text style={styles.errorMessage}>{errorMessage}</Text>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={setWeight}
+                    value={weight}
+                    placeholder="Ex. 75.365"
+                    keyboardType="numeric"
+                />
+                <TouchableOpacity style={styles.buttonCalculator}
+                    onPress={() => {
+                        validationImc()
+                    }}>
+                    <Text style={styles.textButtonCalculator}>{textButton}</Text>
+                </TouchableOpacity>
+            </View>
+            <ResultImc messageResultImc={messageImc} resultImc={imc} />
+        </View>
     );
 }
